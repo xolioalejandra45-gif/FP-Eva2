eb88f93a5fe4af21db41dabf13309375e2ac370b
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-10-03 16:50:58 -0600

    Eva2_8_For_25550473

---
+++ Eva2_8_For/src/eva2_8_for/Eva2_8_For.java
@@ -0,0 +1,17 @@
+package eva2_8_for;
+
+public class Eva2_8_For {
+
+    public static void main(String[] args) {
+        int i;
+//        Incremento
+        for(i = 1 ; i <= 10; i++){
+            System.out.println("Hola mundo");
+        }
+//        Decremento
+        for(i = 10 ; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+    
+}
