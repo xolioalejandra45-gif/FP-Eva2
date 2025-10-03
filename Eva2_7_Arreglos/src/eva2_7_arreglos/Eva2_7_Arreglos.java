4ca7dc248489fc5dba6f1857a80e446fe2872fd1
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-10-03 16:40:12 -0600

    Eva2_7_Arreglos_25550473

---
+++ Eva2_7_Arreglos/src/eva2_7_arreglos/Eva2_7_Arreglos.java
@@ -0,0 +1,14 @@
+package eva2_7_arreglos;
+
+public class Eva2_7_Arreglos {
+
+    public static void main(String[] args) {
+        String cade = "Hola mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        int valor = 10;
+    }
+    
+}
