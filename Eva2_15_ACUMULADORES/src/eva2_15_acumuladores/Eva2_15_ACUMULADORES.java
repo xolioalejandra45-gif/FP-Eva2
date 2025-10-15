0f1098897291ac92546cb090513c2d3401c97ad2
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-10-15 16:43:30 -0600

    Eva2_15_ACUMULADORES_25550473

---
+++ Eva2_15_ACUMULADORES/src/eva2_15_acumuladores/Eva2_15_ACUMULADORES.java
@@ -0,0 +1,21 @@
+
+package eva2_15_acumuladores;
+
+public class Eva2_15_ACUMULADORES {
+
+    public static void main(String[] args) {
+//        Acumuladores: ++ -- += -= *= /=
+            int cuenta = 0; 
+            // = es una asiganción
+            System.out.println(cuenta);
+            cuenta = cuenta + 100; //acumulador
+            System.out.println(cuenta);
+            cuenta = cuenta + 1000;
+            System.out.println(cuenta);
+            cuenta = cuenta - 800; // cuenta -= 800 (es lo mismo)
+            System.out.println(cuenta);
+            cuenta += 2000;
+            System.out.println(cuenta);
+    }
+    
+}
