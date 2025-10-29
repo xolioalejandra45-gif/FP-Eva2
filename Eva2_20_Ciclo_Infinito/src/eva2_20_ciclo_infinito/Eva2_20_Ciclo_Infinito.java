f81f28a6448dd7b74645699eacc981914b5bc776
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-10-29 16:46:24 -0600

    Eva2_20_Ciclo_Infinito_25550473

---
+++ Eva2_20_Ciclo_Infinito/src/eva2_20_ciclo_infinito/Eva2_20_Ciclo_Infinito.java
@@ -0,0 +1,11 @@
+package eva2_20_ciclo_infinito;
+
+public class Eva2_20_Ciclo_Infinito {
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Hola");
+        }
+    }
+    
+}
